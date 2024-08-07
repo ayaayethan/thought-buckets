bb337229c4c46d100ee25e77262800e6d63df5bb
Your Name <root@Ethan.localdomain>
2024-08-07 16:58:31 -0700

    authentication modal

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -16,6 +21,23 @@ export const Navbar = () => {
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex
       items-center gap-x-2">
+        {isLoading && (
+          <p>Loading...</p>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">
+                Get Thought Buckets Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
