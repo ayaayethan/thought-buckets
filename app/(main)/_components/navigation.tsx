6915ac9aed1ae9ff11c5baa68efb6495b498fc1e
Your Name <root@Ethan.localdomain>
2024-08-07 19:34:57 -0700

    collapse

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -60,7 +60,21 @@ export const Navigation = () => {
       navbarRef.current.style.setProperty("width", isMobile ? "0" : "calc(100% - 240px");
       navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
     };
+
     setTimeout(() => setIsResetting(false), 300);
+  }
+
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("width", "100%");
+      navbarRef.current.style.setProperty("left", "0");
+
+      setTimeout(() => setIsResetting(false), 300);
+    }
   }
 
   return (
@@ -74,6 +88,7 @@ export const Navigation = () => {
         )}
       >
         <div
+          onClick={collapse}
           role="button"
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
@@ -105,7 +120,12 @@ export const Navigation = () => {
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
+          {isCollapsed &&
+            <MenuIcon
+              onClick={resetWidth}
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
+            />}
         </nav>
       </div>
     </>
