e9d244488c28d96b8b863eaf018e3d6130341773
Your Name <root@Ethan.localdomain>
2024-08-08 08:56:56 -0700

    log out button

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 
-import { useUser } from "@clerk/clerk-react";
+import { SignOutButton, useUser } from "@clerk/clerk-react";
 
 export const UserItem = () => {
   const { user } = useUser();
@@ -42,7 +42,25 @@ export const UserItem = () => {
           <p className="text-xs font-medium leading-none text-muted-foreground">
             {user?.emailAddresses[0].emailAddress}
           </p>
+          <div className="flex items-center gap-x-2">
+            <div className="rounded-md bg-secondary p-1">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={user?.imageUrl} />
+              </Avatar>
+            </div>
+            <div className="space-y-1">
+              <p className="text-sm line-clamp-1">
+                {user?.fullName}&apos;s Bucket
+              </p>
+            </div>
+          </div>
         </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild className="w-full cursor-pointer text-muted-foreground">
+          <SignOutButton>
+            Log Out
+          </SignOutButton>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
