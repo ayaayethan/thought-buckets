7b0641460131f2abb26e3ba2626652109007f786
Your Name <root@Ethan.localdomain>
2024-08-10 14:17:32 -0700

    dynamic title

--- app/(main)/(routes)/buckets/[bucketId]/page.tsx
+++ app/(main)/(routes)/buckets/[bucketId]/page.tsx
@@ -1,7 +1,41 @@
-const BuckedIdPage = () => {
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+
+interface BucketIdPageProps {
+  params: {
+    bucketId: Id<"buckets">;
+  }
+}
+
+const BuckedIdPage = ({
+  params
+} : BucketIdPageProps ) => {
+  const bucket = useQuery(api.buckets.getById, {
+    bucketId: params.bucketId
+  })
+
+  if (bucket === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
   return (
-    <div>
-      Bucket Page
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
+        <Toolbar initialData={bucket}/>
+      </div>
     </div>
   )
 }
