54c02b0b76be11488b2c4ad723f8f0c09ce348ab
Your Name <root@Ethan.localdomain>
2024-08-11 13:54:40 -0700

    copy published link

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -12,7 +12,7 @@ import { api } from "@/convex/_generated/api";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-import { Globe } from "lucide-react";
+import { Check, Copy, Globe } from "lucide-react";
 
 interface PublishProps {
   initialData: Doc<"buckets">
@@ -84,8 +84,31 @@ export const Publish = ({
       </PopoverTrigger>
       <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
           {initialData.isPublished ? (
-            <div>
-              Published
+            <div className="space-y-4">
+              <div className="flex items-center gap-x-2">
+                <Globe className="text-sky-500 animate-pulse h-4 w-4" />
+                <p className="text-xs font-medium text-sky-500">
+                  This note is live on the web.
+                </p>
+              </div>
+              <div className="flex items-center">
+                <input
+                  className="flex-1 px-2 text-xs border rounded-l-md h-8 bg-muted truncate"
+                  value={url}
+                  disabled
+                />
+                <Button
+                  onClick={onCopy}
+                  disabled={copied}
+                  className="h-8 rounded-l-none"
+                >
+                  {copied ? (
+                    <Check className="h-4 w-4"/>
+                  ) : (
+                    <Copy className="h-4 w-4"/>
+                  )}
+                </Button>
+              </div>
             </div>
           ) : (
             <div className="flex flex-col items-center justify-center">
